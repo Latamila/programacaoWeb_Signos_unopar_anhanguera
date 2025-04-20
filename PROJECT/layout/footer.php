60409a141feb0769e95468b592200eec56bf4dbe
Camila <87709951+Latamila@users.noreply.github.com>
2025-04-19 23:03:47 -0300

    Update footer.php

--- PROJECT/layout/footer.php
+++ PROJECT/layout/footer.php
@@ -1,5 +1,5 @@
 <footer class="text-center mt-4">
-    <p>&copy; <?php echo date("Y"); ?> Descubra Seu Signo. Todos os direitos reservados.</p>
+    <p>&copy; <?php echo date("Y"); ?> Feito por Camila Veloso. Doações pelo Pix 31971160847.</p>
     <div>
         <a href="https://www.instagram.com" target="_blank">Instagram</a> |
         <a href="https://www.facebook.com" target="_blank">Facebook</a> |
